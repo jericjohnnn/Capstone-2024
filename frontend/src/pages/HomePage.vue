06899f20585ecf4fff68b3d95685086136a7c04a
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2024-10-17 19:56:56 +0800

    configured frontend to setup auth

---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="w-full px-4 sm:px-6 md:px-8 ">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  </main>
+</template>
