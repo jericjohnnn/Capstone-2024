1f9985caa2e02c8ce8c68132a79ce9ee1f03e473
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2024-10-18 19:34:42 +0800

    organized validation to requests folder

---
+++ backend/app/Http/Requests/Auth/RegisterUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_type' => 'required|exists:user_types,id',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'birthdate' => 'required|date',
+            'gender' => 'in:male,female,other',
+            'contact_number' => 'required|string',
+        ];
+    }
+}
