388efb8ad168e7dec4799c8b5ded184764db802c
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2024-10-18 18:07:38 +0800

    added modular routes and added mockups to some pages that are ready for actual data

---
+++ frontend/src/router/tutorRoutes.js
@@ -0,0 +1,12 @@
+import TutorProfilePage from "@/pages/tutor/TutorProfilePage.vue";
+
+
+const tutorRoutes = [
+  {
+    path: '/tutor/profile',
+    name: 'TutorProfile',
+    component: TutorProfilePage
+  },
+];
+
+export default tutorRoutes;
